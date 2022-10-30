43a0d732b321d37690b3eba5fd0470715973d505
MatokoLB <matheuslisboabr@gmail.com>
2022-10-29 23:40:56 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+// import jsonServer from 'json-server';
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json');
+// const middlewares = jsonServer.defaults();
+// const port = process.env.Port || 3200;
+
+// server.use(middlewares);
+// server.use(router);
+
+// server.listen(port);
+
+// server.js
+import jsonServer from 'json-server'
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('Servidor ativo')
+})
